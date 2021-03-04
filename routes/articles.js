c59367a0d8143491e334b358b75234b2a31a2191
Fardin Ahmed Niloy <niloy98.it@gmail.com>
2021-03-05 01:42:03 +0600

    added slugify

--- routes/articles.js
+++ routes/articles.js
@@ -6,8 +6,8 @@ router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() });
 });
 
-router.get('/:id', async (req, res) => {
-  const article = await Article.findById(req.params.id);
+router.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) res.redirect('/');
   res.render('articles/show', { article: article });
 });
